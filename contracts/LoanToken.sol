fd3b94242b5332f3d43c6c44bc766ceaf8dc9dcf
Pol Sendra <pol@hero-fintech.com>
2019-06-05 10:31:38 +0200

    more things

--- contracts/LoanToken.sol
+++ contracts/LoanToken.sol
@@ -9,9 +9,14 @@ import './Authorization.sol';
 contract LoanToken is ERC721Mintable, ERC721Burnable, ERC721KYC {
     Authorization auth;
 
+    modifier onlyKYC { // should it be in its own separete contract ??
+        require(auth.isKYCConfirmed(msg.sender), 'user does not have KYC');
+        _;
+    }
+
     constructor(address authAddress, address minterAddress) public {
         auth = Authorization(authAddress);
-
+        // what to do with minterAddress ???
     }
 
     function mint(address to, uint256 tokenId) public onlyMinter {}
