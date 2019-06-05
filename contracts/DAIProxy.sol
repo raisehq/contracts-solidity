433921c4dd6b53f2201d96989a546a4c9c04da7b
Pol Sendra <pol@hero-fintech.com>
2019-06-05 18:03:15 +0200

    uncomented code

--- contracts/DAIProxy.sol
+++ contracts/DAIProxy.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
 import './Authorization.sol';
-// import './LoanContract.sol';
+import './LoanContract.sol';
 
 contract DAIProxy {
     ERC20 DAIToken;
@@ -19,8 +19,8 @@ contract DAIProxy {
     function fund(address loanAddress, uint256 fundingAmount) public onlyKYCanFund onlyHasDepositCandFund  {
         transfer(loanAddress, fundingAmount);
 
-        // LoanContract loanContract = LoanContract(loanAddress);
-        // loanContract.onFundingReceived(msg.sender, fundingAmount);
+        LoanContract loanContract = LoanContract(loanAddress);
+        loanContract.onFundingReceived(msg.sender, fundingAmount);
 
         emit LoanFunded(msg.sender, loanAddress, fundingAmount);
     }
@@ -28,8 +28,8 @@ contract DAIProxy {
     function repay(address loanAddress, uint256 repaymentAmount) public onlyKYCanFund {
         transfer(loanAddress, repaymentAmount);
 
-        // LoanContract loanContract = LoanContract(loanAddress);
-        // loanContract.onRepaymentReceived(msg.sender, repaymentAmount);
+        LoanContract loanContract = LoanContract(loanAddress);
+        loanContract.onRepaymentReceived(msg.sender, repaymentAmount);
 
         emit RepaymentReceived(msg.sender, loanAddress, repaymentAmount);
     }
