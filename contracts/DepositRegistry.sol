8e4d90822f8cac0b3c217b6f429c675a73d470a8
Иван Камакин <ivkamakin@edu.hse.ru>
2019-07-01 06:58:17 +0300

    LP-425/Update DepositRegistry.sol to work with ReferralTracker

--- contracts/DepositRegistry.sol
+++ contracts/DepositRegistry.sol
@@ -2,18 +2,25 @@ pragma solidity ^0.5.0;
 
 import 'openzeppelin-solidity/contracts/ownership/Ownable.sol';
 import 'openzeppelin-solidity/contracts/token/ERC20/ERC20.sol';
+import './ReferralTracker.sol';
+import './KYCRegistry.sol';
 
 contract DepositRegistry is Ownable {
   mapping(address => bool) deposited;
   uint256 DEPOSIT_AMNT = 200000000000000000000;
   ERC20 token;
 
+  KYCRegistry kyc;
+  ReferralTracker ref;
+
   event ProxyDepositCompleted(address indexed user);
   event UserDepositCompleted(address indexed user);
   event UserWithdrawnCompleted(address indexed user);
 
-  constructor(address tokenAddress) public {
+  constructor(address tokenAddress, address referralAddress, address kycAddress) public {
     token = ERC20(tokenAddress);
+    ref = ReferralTracker(referralAddress);
+    kyc = KYCRegistry(kycAddress);
   }
 
   function depositFor(address from) public {
@@ -27,8 +34,24 @@ contract DepositRegistry is Ownable {
     emit ProxyDepositCompleted(from);
   }
 
+  function depositForWithReferral(address from, address referrer) public {
+    require(deposited[from] == false, 'alredy deposited');
+    require(token.allowance(from, address(this)) >= DEPOSIT_AMNT, 'address not approved amount');
+    require(msg.sender == from, 'cannot deposit with a referral from another address');
+
+    deposited[from] = true;
+    token.transferFrom(from, address(this), DEPOSIT_AMNT);
+
+    ref.registerReferral(referrer);
+
+    emit UserDepositCompleted(from);
+    emit ProxyDepositCompleted(from);
+  }
+
   function withdraw(address to) public {
     require(deposited[to], 'address not deposited');
+    require(kyc.isConfirmed(msg.sender), 'cannot withdraw without KYC');
+
     deposited[to] = false;
     token.transfer(to, DEPOSIT_AMNT);
     emit UserWithdrawnCompleted(to);
