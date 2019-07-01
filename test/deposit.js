b4521580a73e53aefc8e1130c858fd084a50af76
Иван Камакин <ivkamakin@edu.hse.ru>
2019-07-01 18:53:10 +0300

    LP-425/Add test to prevent the vulnerability reappearing

--- test/deposit.js
+++ test/deposit.js
@@ -8,24 +8,38 @@ const HeroFakeTokenContract = artifacts.require('HeroFakeToken');
 const HeroAmount = '200000000000000000000';
 
 contract('Deposit Contract', function (accounts) {
- 
+
   let token
   let HeroToken
   let DepositRegistry;
 
-  const owner = accounts[0]
-  const user = accounts[1]
+  const owner = accounts[0];
+  const user = accounts[1];
 
   describe('deploy', () => {
     it('should be able to deploy and create associated token contract', async () => {
-      HeroToken = await HeroFakeTokenContract.new()
+      HeroToken = await HeroFakeTokenContract.new();
       DepositRegistry = await DepositRegistryContract.new(HeroToken.address,  { from: owner });
       await HeroToken.transferFakeHeroTokens(user);
       await HeroToken.approve(DepositRegistry.address, HeroAmount,{ from: user });
 
       await DepositRegistry.depositFor(user, { from: owner });
       assert.equal(await DepositRegistry.hasDeposited(user), true);
-      
+
+    });
+
+    it('should not be able to successfully call withdraw if the message sender is not deposited', async () => {
+      HeroToken = await HeroFakeTokenContract.new();
+      DepositRegistry = await DepositRegistryContract.new(HeroToken.address,  { from: owner });
+      await HeroToken.transferFakeHeroTokens(user);
+      await HeroToken.approve(DepositRegistry.address, HeroAmount,{ from: user });
+
+      await DepositRegistry.depositFor(user, { from: owner });
+      try {
+        DepositRegistry.withdraw(owner, {from: owner});
+      } catch (error) {
+        expect(error).to.not.equal(undefined);
+      }
     });
   });
-})
+});
