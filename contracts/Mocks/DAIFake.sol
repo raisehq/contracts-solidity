51fe925f68cbb2024556d9ab1c7853bc66dcbd9a
Daniel Martin <daniel@herotoken.io>
2020-03-05 17:34:54 +0100

    LP-1090 bad creation of fake tokens.

--- contracts/Mocks/DAIFake.sol
+++ contracts/Mocks/DAIFake.sol
@@ -5,9 +5,9 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract DAIFake is ERC20Mintable {
     using SafeMath for uint256;
-    string public constant NAME = "DAI Fake Token";
-    string public constant SYMBOL = "DAIFake";
-    uint8 public constant DECIMALS = 18;
+    string public constant name = "DAI Fake Token";
+    string public constant symbol = "DAIF";
+    uint8 public constant decimals = 18;
 
     // 1000 tokens == 1000000000000000000000 in 18 decimal (Like wei)
     constructor() public {
