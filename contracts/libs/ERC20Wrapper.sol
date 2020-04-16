8ad2b53f0b997b17b1c5b21401d7e947cccb6e9a
Daniel Martin <daniel@herotoken.io>
2020-04-16 13:00:47 +0200

    LP-1229 fix USDT

--- contracts/libs/ERC20Wrapper.sol
+++ contracts/libs/ERC20Wrapper.sol
@@ -1,66 +1,101 @@
 pragma solidity 0.5.12;
 
-interface IERC20Wrapper {
+
+interface IERC20NotStandar {
+    function transfer(address _to, uint256 _quantity) external;
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _quantity
+    ) external;
+
+    function approve(address _spender, uint256 _quantity) external;
+}
+
+
+interface IERC20 {
     function totalSupply() external view returns (uint256);
+
     function balanceOf(address _owner) external view returns (uint256);
+
     function allowance(address _owner, address _spender) external view returns (uint256);
+
     function transfer(address _to, uint256 _quantity) external returns (bool);
-    function transferFrom(address _from, address _to, uint256 _quantity) external returns (bool);
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _quantity
+    ) external returns (bool);
+
     function approve(address _spender, uint256 _quantity) external returns (bool);
+
     function symbol() external view returns (string memory);
 }
 
+
 library ERC20Wrapper {
     function balanceOf(address _token, address _owner) external view returns (uint256) {
-        return IERC20Wrapper(_token).balanceOf(_owner);
+        return IERC20(_token).balanceOf(_owner);
     }
 
-    function allowance(address _token, address owner, address spender)
-        external
-        view
-        returns (uint256)
-    {
-        return IERC20Wrapper(_token).allowance(owner, spender);
+    function allowance(
+        address _token,
+        address owner,
+        address spender
+    ) external view returns (uint256) {
+        return IERC20(_token).allowance(owner, spender);
     }
 
-    function transfer(address _token, address _to, uint256 _quantity) external returns (bool) {
+    function transfer(
+        address _token,
+        address _to,
+        uint256 _quantity
+    ) external returns (bool) {
         if (isIssuedToken(_token)) {
-            IERC20Wrapper(_token).transfer(_to, _quantity);
+            IERC20NotStandar(_token).transfer(_to, _quantity);
 
             require(checkSuccess(), "ERC20Wrapper.transfer: Bad return value");
             return true;
         } else {
-            return IERC20Wrapper(_token).transfer(_to, _quantity);
+            return IERC20(_token).transfer(_to, _quantity);
         }
     }
 
-    function transferFrom(address _token, address _from, address _to, uint256 _quantity)
-        external
-        returns (bool)
-    {
+    function transferFrom(
+        address _token,
+        address _from,
+        address _to,
+        uint256 _quantity
+    ) external returns (bool) {
         if (isIssuedToken(_token)) {
-            IERC20Wrapper(_token).transferFrom(_from, _to, _quantity);
+            IERC20NotStandar(_token).transferFrom(_from, _to, _quantity);
             // Check that transferFrom returns true or null
             require(checkSuccess(), "ERC20Wrapper.transferFrom: Bad return value");
             return true;
         } else {
-            return IERC20Wrapper(_token).transferFrom(_from, _to, _quantity);
+            return IERC20(_token).transferFrom(_from, _to, _quantity);
         }
     }
 
-    function approve(address _token, address _spender, uint256 _quantity) external returns (bool) {
+    function approve(
+        address _token,
+        address _spender,
+        uint256 _quantity
+    ) external returns (bool) {
         if (isIssuedToken(_token)) {
-            IERC20Wrapper(_token).approve(_spender, _quantity);
+            IERC20NotStandar(_token).approve(_spender, _quantity);
             // Check that approve returns true or null
             require(checkSuccess(), "ERC20Wrapper.approve: Bad return value");
             return true;
         } else {
-            return IERC20Wrapper(_token).approve(_spender, _quantity);
+            return IERC20(_token).approve(_spender, _quantity);
         }
     }
 
     function isIssuedToken(address _token) private view returns (bool) {
-        return (keccak256(abi.encodePacked((IERC20Wrapper(_token).symbol()))) ==
+        return (keccak256(abi.encodePacked((IERC20(_token).symbol()))) ==
             keccak256(abi.encodePacked(("USDT"))));
     }
 
@@ -96,6 +131,6 @@ library ERC20Wrapper {
         }
 
         // check if returned value is one or nothing
-        return returnValue == 1;
+        return returnValue != 0;
     }
 }
