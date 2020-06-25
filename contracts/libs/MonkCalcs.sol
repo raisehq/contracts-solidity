ca68ca812fd232dfcb2b1bba5976ee8f095f77d9
Kartojal <canillas.mail@gmail.com>
2020-06-25 15:32:26 +0200

    LP-1249 wip mulDic

---
+++ contracts/libs/MonkCalcs.sol
@@ -0,0 +1,36 @@
+pragma solidity ^0.5.0;
+
+library MonkCalcs {
+    function fullMul(uint256 x, uint256 y) public pure returns (uint256 l, uint256 h) {
+        uint256 mm = mulmod(x, y, uint256(-1));
+        l = x * y;
+        h = mm - l;
+        if (mm < l) h -= 1;
+    }
+
+    function mulDiv(
+        uint256 x,
+        uint256 y,
+        uint256 z
+    ) public pure returns (uint256) {
+        (uint256 l, uint256 h) = fullMul(x, y);
+        require(h < z);
+        uint256 mm = mulmod(x, y, z);
+        if (mm > l) h -= 1;
+        l -= mm;
+        uint256 pow2 = z & -z;
+        z /= pow2;
+        l /= pow2;
+        l += h * ((-pow2) / pow2 + 1);
+        uint256 r = 1;
+        r *= 2 - z * r;
+        r *= 2 - z * r;
+        r *= 2 - z * r;
+        r *= 2 - z * r;
+        r *= 2 - z * r;
+        r *= 2 - z * r;
+        r *= 2 - z * r;
+        r *= 2 - z * r;
+        return l * r;
+    }
+}
